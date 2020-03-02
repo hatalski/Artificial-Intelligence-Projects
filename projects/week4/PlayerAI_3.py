7c67ebda7b181578530ddd30397f40672e28dd8e
VH <284556+hatalski@users.noreply.github.com>
2020-03-02 21:54:53 +0300

    implemented minimax algo, but not used yet

---
+++ projects/week4/PlayerAI_3.py
@@ -0,0 +1,47 @@
+from random import randint
+from BaseAI_3 import BaseAI
+
+class PlayerAI(BaseAI):
+    def getMove(self, grid):
+        """
+        The getMove() function, which you will need to implement, 
+        returns a number that indicates the player’s action. 
+        In particular, 0 stands for "Up", 1 stands for "Down", 2 stands for "Left", 
+        and 3 stands for "Right".
+        """
+        cells = grid.getAvailableCells()
+        print(cells)
+
+        print(grid)
+
+        return cells[randint(0, len(cells) - 1)] if cells else None
+
+    def minimax_decision(self, state):
+      child, _ = self.maximize(state)
+      return child
+
+    def minimize(self, state):
+      if terminal_test(state):
+        return (None, state)
+
+      min_child, min_utility = None, math.inf
+
+      for child in state.children():
+        _, utility = self.maximize(child)
+        if utility < min_utility:
+          min_child, min_utility = child, utility
+      
+      return min_child, min_utility
+
+    def maximize(self, state):
+      if terminal_test(state):
+        return (None, state)
+
+      max_child, max_utility = None, -math.inf
+
+      for child in state.children():
+        _, utility = self.minimize(child)
+        if utility > max_utility:
+          max_child, max_utility = child, utility
+      
+      return max_child, max_utility
